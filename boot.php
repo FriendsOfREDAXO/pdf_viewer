91e88c4994d18f3af3e384532ee8d046aad40e9e
Christian <31507654+chrison94@users.noreply.github.com>
2017-11-20 11:34:53 +0100

    oops
    Zeugs entfernt

--- boot.php
+++ boot.php
@@ -1,16 +1,10 @@
 <?php
 
-$this->setProperty('author', 'Friends Of REDAXO');
+$this->setProperty('author', 'KLXM');
 
 if (rex::isBackend() && is_object(rex::getUser())) {
-    rex_perm::register('demo_addon[]');
-    rex_perm::register('demo_addon[config]');
+
 }
 if (rex::isBackend() && rex::getUser()) {
 
-    rex_view::addCssFile($this->getAssetsUrl('css/style.css?v=' . $this->getVersion()));
-
-    if (rex_be_controller::getCurrentPagePart(2) == 'config') {
-        rex_view::addJsFile($this->getAssetsUrl('js/script.js?v=' . $this->getVersion()));
-    }
 }
